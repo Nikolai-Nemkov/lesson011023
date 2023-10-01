6b91d874fcb7c128ae62e3e0f21f7a0e6dec1904
Николай <nnm_58@mail.ru>
2023-10-01 14:23:27 +0300

    Создал папку Task31

---
+++ Task35/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов.
+// В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] GenerateArray(int min, int max, int size)
+{
+    int[] arr = new int[size];
+    var rnd = new Random();
+
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+void PrintArrayBr(int[] arr)
+{
+ Console.Write("[");
+    for(int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}");
+    Console.Write("]");
+}
+
+int CountInInterval(int[] arr, int min, int max)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] >= min && arr[i] <= max) count++;
+    }
+    return count;
+}
+
+int[] array = GenerateArray(-10, 10, 23);
+PrintArrayBr(array);
+int count = CountInInterval(array, 10, 99);
+Console.WriteLine($" -> {count}");
+
